fdb2a2d5865eacb77a5479fd93a2a53521844509
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-08-07 14:09:46 +0530

    printing of palindrom string

---
+++ Main.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static boolean isPalindrom(String str){
+
+        int left=0;
+        int right=str.length()-1;
+
+        while(left<right){
+
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+
+        for(int si=0;si<str.length();si++){
+            for(int ei=si;ei<str.length();ei++){
+                if(isPalindrom(str.substring(si,ei+1))){
+                    System.out.println(str.substring(si,ei+1));
+                }
+            }
+        }
+
+    }
+}
