5f7d2afac9d16ed146a22e75041477aae898e9b7
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-08-14 12:49:03 +0530

    string compress

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-package com.company;
+// PRINTING OF PALINDROM STRING
+
+/*package com.company;
 import java.util.*;
 
 public class Main {
@@ -33,4 +35,66 @@ public class Main {
         }
 
     }
+}*/
+
+//------------------------------------------------------------------------------------------------------
+
+// COMPRESS STRING
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main{
+
+    public static String compress1(String str){
+
+        String comp1=""+str.charAt(0);
+        for(int i=0;i<str.length();i++){
+            if(comp1.charAt(comp1.length()-1)!=str.charAt(i)){
+                comp1=comp1+str.charAt(i);
+            }
+        }
+        return comp1;
+    }
+
+    public static String compress2(String str){
+
+        String comp2=""+str.charAt(0);
+        int count=1;
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==str.charAt(i-1)){
+                count++;
+            }
+            else{
+                if(count>1){
+                    comp2=comp2+count+str.charAt(i);
+                    count=1;
+                }
+                else{
+                    comp2+=str.charAt(i);
+                }
+            }
+        }
+
+        if(count>1){
+            comp2=comp2+count;
+        }
+
+        return comp2;
+    }
+
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+
+        String c1=compress1(str);
+        String c2=compress2(str);
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+
 }
+
+//------------------------------------------------------------------------------------------------------
